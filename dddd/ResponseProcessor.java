14c5b7584915bd8164de457ee994a73946bebb68
root <root@karavan-64889b5668-6vj5w>
2023-01-11 17:20:09 +0000

    2023-01-11T17:20:09.295248: dddd

--- dddd/ResponseProcessor.java
+++ dddd/ResponseProcessor.java
@@ -1,6 +1,5 @@
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
-import org.apache.camel.component.netty.NettyConstants;
 import javax.inject.Named;
 import javax.inject.Singleton;
 
@@ -10,7 +9,7 @@ public class ResponseProcessor implements Processor {
 
   public void process(Exchange exchange) throws Exception {
     if(exchange.getIn().getBody().equals("quit")) {
-       exchange.getOut().setHeader(NettyConstants.NETTY_CLOSE_CHANNEL_WHEN_COMPLETE, true);
+       exchange.getOut().setHeader("CamelNettyCloseChannelWhenComplete", true);
     }
     else {
        exchange.getOut().setBody("Hello from Karavan Pod");

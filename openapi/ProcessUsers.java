a3a953071d95c6b9c74ed82f8820356ce739edb0
root <root@karavan-7dcb9c9cb5-ktfx4>
2023-01-17 17:58:16 +0000

    2023-01-17T17:58:16.904759: openapi

---
+++ openapi/ProcessUsers.java
@@ -0,0 +1,14 @@
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+@Singleton
+@Named("ProcessUsers")
+public class ProcessUsers implements Processor {
+
+  public void process(Exchange exchange) throws Exception {
+      exchange.getOut().setBody("{\"username\":\"mrinal\"}");
+  }
+}

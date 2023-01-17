8eec1f3ab3b9dba81110bd5cfb1b6187f36506b4
root <root@karavan-7dcb9c9cb5-ktfx4>
2023-01-17 21:01:02 +0000

    2023-01-17T21:01:02.251007: openapi

--- openapi/ProcessUsers.java
+++ openapi/ProcessUsers.java
@@ -10,6 +10,6 @@ public class ProcessUsers implements Processor {
 
   public void process(Exchange exchange) throws Exception {
       exchange.getOut().setBody("{\"username\":\"Karavan\"}");
-      exchange.getOut().setHeader(Exchange.HTTP_RESPONSE_CODE, "403");
+      exchange.getOut().setHeader(Exchange.HTTP_RESPONSE_CODE, "401");
   }
 }

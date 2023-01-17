ae66e3e1cf9e574dacd2fe2caece213db7b62d76
root <root@karavan-7dcb9c9cb5-ktfx4>
2023-01-17 22:06:03 +0000

    2023-01-17T22:06:03.019046: openapi

--- openapi/ProcessUsers.java
+++ openapi/ProcessUsers.java
@@ -11,16 +11,19 @@ public class ProcessUsers implements Processor {
   public void process(Exchange exchange) throws Exception {
       if(exchange.getIn().getHeader(Exchange.HTTP_METHOD).equals("POST")) { 
          System.out.println("Post method :" + exchange.getIn().getBody());
+         exchange.getOut().setHeader(Exchange.HTTP_RESPONSE_CODE, "202");
       }
       else  if(exchange.getIn().getHeader(Exchange.HTTP_METHOD).equals("DELETE")) {
          System.out.println("DELETE method :" + exchange.getIn().getBody());
+         exchange.getOut().setHeader(Exchange.HTTP_RESPONSE_CODE, "200");
       }
       else  if(exchange.getIn().getHeader(Exchange.HTTP_METHOD).equals("GET")) {
         if(!exchange.getIn().getHeader(Exchange.HTTP_PATH, String.class).isEmpty()) {
           System.out.println("Received Get with HTTP_PATH:" + exchange.getIn().getHeader(Exchange.HTTP_PATH));
+          exchange.getOut().setBody("{\"username\":\"Karavan_PATH\"}");
         }
         exchange.getOut().setBody("{\"username\":\"Karavan\"}");
+        exchange.getOut().setHeader(Exchange.HTTP_RESPONSE_CODE, "200");
       }
-      exchange.getOut().setHeader(Exchange.HTTP_RESPONSE_CODE, "200");
   }
 }

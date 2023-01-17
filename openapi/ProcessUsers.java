0e834214fe43897a7dc062261ca208d59a0bc9ee
root <root@karavan-7dcb9c9cb5-ktfx4>
2023-01-17 21:34:07 +0000

    2023-01-17T21:34:07.658052: openapi

--- openapi/ProcessUsers.java
+++ openapi/ProcessUsers.java
@@ -9,7 +9,18 @@ import javax.inject.Singleton;
 public class ProcessUsers implements Processor {
 
   public void process(Exchange exchange) throws Exception {
-      exchange.getOut().setBody("{\"username\":\"Karavan\"}");
+      if(exchange.getIn().getHeader(Exchange.HTTP_METHOD).equalc("POST")) { 
+         System.out.println("Post method :" + exchange.getIn().getBody());
+      }
+      else  if(exchange.getIn().getHeader(Exchange.HTTP_METHOD).equalc("DELETE")) {
+         System.out.println("DELETE method :" + exchange.getIn().getBody());
+      }
+      else  if(exchange.getIn().getHeader(Exchange.HTTP_METHOD).equals("GET")) {
+        if(!exchange.getIn().getHeader(Exchange.HTTP_QUERY).isEmpty()) {
+          System.out.println("Received Get with Parameter:" + exchange.getIn().getHeader(Exchange.HTTP_QUERY));
+        }
+        exchange.getOut().setBody("{\"username\":\"Karavan\"}");
+      }
       exchange.getOut().setHeader(Exchange.HTTP_RESPONSE_CODE, "200");
   }
 }
